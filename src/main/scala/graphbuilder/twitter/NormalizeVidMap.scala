4f232dd0d032b947f9473200ed1facc0281a3a4c
Jay Gu <gu.haijie@gmail.com>
2012-10-23 15:50:24 -0400

    switch to broadcast local map

--- src/main/scala/graphbuilder/twitter/NormalizeVidMap.scala
+++ src/main/scala/graphbuilder/twitter/NormalizeVidMap.scala
@@ -7,20 +7,20 @@ import scala.collection.mutable.HashMap
 
 object NormalizeVidMap {
   def uniqIds(sc: SparkContext, vertexlist: spark.RDD[String]) : spark.RDD[String] = {
-    (vertexlist.map (w => (w, 1))) reduceByKey(_+_, 256) map {case (x, _) => x} 
+    (vertexlist.map (w => (w, 1))) reduceByKey(_+_, 32) map {case (x, _) => x} 
   }
   
   def translateEdge(sc: SparkContext, vidmap: spark.RDD[(String, Int)], edgelist: spark.RDD[(String, String)]) :
   	spark.RDD[(Int, Int)] = {
-    val transSource = vidmap.join(edgelist, 256).map { case (source, (sourceid, target)) => (target, sourceid)}
-    val transTarget = transSource.join(vidmap, 256).map { case (target, (targetid, sourceid)) => (sourceid, targetid)}
+    val transSource = vidmap.join(edgelist, 32).map { case (source, (sourceid, target)) => (target, sourceid)}
+    val transTarget = transSource.join(vidmap, 32).map { case (target, (targetid, sourceid)) => (sourceid, targetid)}
     transTarget 
   }
   
     def translateEdgeWithData[T](sc: SparkContext, vidmap: spark.RDD[(String, Int)],
         edgelist: spark.RDD[((String), (String, T))]) : spark.RDD[(Int, Int, T)] = {
-    val transSource = vidmap.join(edgelist, 256).map { case (source, (sourceid, (target, data))) => (target, (sourceid, data))}
-    val transTarget = vidmap.join(transSource, 256).map { case (target, (targetid, (sourceid, data))) => (sourceid, targetid, data)}
+    val transSource = vidmap.join(edgelist, 32).map { case (source, (sourceid, (target, data))) => (target, (sourceid, data))}
+    val transTarget = vidmap.join(transSource, 32).map { case (target, (targetid, (sourceid, data))) => (sourceid, targetid, data)}
     transTarget 
   }
 }
